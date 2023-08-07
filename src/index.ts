9cbf44bb5aa9aa7be0d18f81a55c754bc023b235
atorber <atorber@163.com>
2023-08-08 00:16:06 +0800

    增加定时任务解析

--- src/index.ts
+++ src/index.ts
@@ -66,65 +66,76 @@ async function onMessage (msg: Message) {
 
     if (text[0] === '/') {
       const textArr = text.split(' ')
-      if (textArr[0] === '/llm' && textArr.length > 1) {
-        log.info('管理员或管理群消息', talkerName, topic)
-        const res: MessageActions = await messageStructuring(text.replace('/llm', ''))
-        log.info('LLM识别结果:', JSON.stringify(res))
+      if (textArr.length) {
+        const cmd = textArr[0]
+        if (cmd === '/llm' && textArr.length > 1) {
+          log.info('管理员或管理群消息', talkerName, topic)
+          const res: MessageActions = await messageStructuring(text.replace('/llm', ''))
+          log.info('LLM识别结果:', JSON.stringify(res))
 
-        if (res.actions.length) {
-          const textMsg:Action|undefined = res.actions[0]
-          const successList = []
-          const failList = []
-          if (textMsg?.actionType === 'sendMessage' && textMsg.event.contacts.length) {
-            const relpText = textMsg.event.text + `\n————From:${msg.talker().name()}`
-            const contacts = textMsg.event.contacts
-            for (const i in contacts) {
-              const curContact = contacts[i]
-              let toUser = await bot.Contact.find({ alias:curContact })
-              if (!toUser) {
-                toUser = await bot.Contact.find({ name:curContact })
+          if (res.actions.length) {
+            const textMsg:Action|undefined = res.actions[0]
+            const successList = []
+            const failList = []
+            if (textMsg?.actionType === 'sendMessage' && textMsg.event.contacts.length) {
+              const relpText = textMsg.event.text + `\n————From:${msg.talker().name()}`
+              const contacts = textMsg.event.contacts
+              for (const i in contacts) {
+                const curContact = contacts[i]
+                let toUser = await bot.Contact.find({ alias:curContact })
+                if (!toUser) {
+                  toUser = await bot.Contact.find({ name:curContact })
+                }
+                log.info('toUser:', JSON.stringify(toUser))
+                if (toUser) {
+                  await toUser.say(relpText)
+                  successList.push(curContact)
+                } else {
+                  failList.push(curContact)
+                }
               }
-              log.info('toUser:', JSON.stringify(toUser))
-              if (toUser) {
-                await toUser.say(relpText)
-                successList.push(curContact)
+              const timeString = new Date().toLocaleString()
+              if (contacts.length === successList.length) {
+                await msg.say(`${timeString}\n${talkerName} /llm >\n全部发送成功[${successList.length}]：${successList.join('、')}`)
+              } else if (contacts.length === failList.length) {
+                await msg.say(`${timeString}\n${talkerName} /llm >\n全部发送失败[${failList.length}]：${failList.join('、')}`)
               } else {
-                failList.push(curContact)
+                await msg.say(`${timeString}\n${talkerName} /llm >\n发送成功[${successList.length}/${contacts.length}]：${successList.join('、')}\n发送失败[${failList.length}/${contacts.length}]：${failList.join('、')}`)
               }
-            }
-            const timeString = new Date().toLocaleString()
-            if (contacts.length === successList.length) {
-              await msg.say(`${talkerName} /llm >\n${timeString}\n全部发送成功[${successList.length}]：${successList.join('、')}`)
-            } else if (contacts.length === failList.length) {
-              await msg.say(`${talkerName} /llm >\n${timeString}\n全部发送失败[${failList.length}]：${failList.join('、')}`)
+            } else if (textMsg?.actionType === 'sendRoomMessage' && textMsg.event.rooms.length) {
+              const relpText = textMsg.event.text + `\n————From:${msg.talker().name()}`
+              const rooms = textMsg.event.rooms
+              for (const i in rooms) {
+                const curRoom = rooms[i]
+                const toUser = await bot.Room.find({ topic:curRoom })
+
+                log.info('toUser:', JSON.stringify(toUser))
+                if (toUser) {
+                  await toUser.say(relpText)
+                  successList.push(curRoom)
+                } else {
+                  failList.push(curRoom)
+                }
+              }
+              const timeString = new Date().toLocaleString()
+              if (rooms.length === successList.length) {
+                await msg.say(`${timeString}\n${talkerName} /llm >\n全部发送成功[${successList.length}]：${successList.join('、')}`)
+              } else if (rooms.length === failList.length) {
+                await msg.say(`${timeString}\n${talkerName} /llm >\n全部发送失败[${failList.length}]：${failList.join('、')}`)
+              } else {
+                await msg.say(`${timeString}\n${talkerName} /llm >\n发送成功[${successList.length}/${rooms.length}]：${successList.join('、')}\n发送失败[${failList.length}/${rooms.length}]：${failList.join('、')}`)
+              }
             } else {
-              await msg.say(`${talkerName} /llm >\n${timeString}\n发送成功[${successList.length}/${contacts.length}]：${successList.join('、')}\n发送失败[${failList.length}/${contacts.length}]：${failList.join('、')}`)
+              const timeString = new Date().toLocaleString()
+              await msg.say(`${timeString}\n${talkerName} /llm >\n${JSON.stringify(textMsg, undefined, 2)}`)
             }
           }
-          if (textMsg?.actionType === 'sendRoomMessage' && textMsg.event.rooms.length) {
-            const relpText = textMsg.event.text + `\n————From:${msg.talker().name()}`
-            const rooms = textMsg.event.rooms
-            for (const i in rooms) {
-              const curRoom = rooms[i]
-              const toUser = await bot.Room.find({ topic:curRoom })
+        }
 
-              log.info('toUser:', JSON.stringify(toUser))
-              if (toUser) {
-                await toUser.say(relpText)
-                successList.push(curRoom)
-              } else {
-                failList.push(curRoom)
-              }
-            }
-            const timeString = new Date().toLocaleString()
-            if (rooms.length === successList.length) {
-              await msg.say(`${talkerName} /llm >\n${timeString}\n全部发送成功[${successList.length}]：${successList.join('、')}`)
-            } else if (rooms.length === failList.length) {
-              await msg.say(`${talkerName} /llm >\n${timeString}\n全部发送失败[${failList.length}]：${failList.join('、')}`)
-            } else {
-              await msg.say(`${talkerName} /llm >\n${timeString}\n发送成功[${successList.length}/${rooms.length}]：${successList.join('、')}\n发送失败[${failList.length}/${rooms.length}]：${failList.join('、')}`)
-            }
-          }
+        // TBD——增加退出指令，当接收到退出指令时，清空cmd
+        if (cmd && [ '/esc', '/exit', '/quit' ].includes(cmd)) {
+          log.info('用户输入了退出指令：', cmd)
+          await msg.say(`${new Date().toLocaleString()}\n${talker.name()} / >\n输入/help查询可用操作指令`)
         }
       }
     }
--- src/types/messageActionsSchema.ts
+++ src/types/messageActionsSchema.ts
@@ -1,4 +1,28 @@
-// The following types define the structure of an object of type MessageActions that represents a list of requested message actions
+// 以下类型定义MessageActions类型的对象的结构，该对象表示请求的消息操作的列表
+
+// 【2023年8月8日晚上10点】提醒【大李】：明天上午8点去接王总
+export type ScheduledTasksMessage = {
+    // Some message content, such as coming to the meeting immediately and picking up the delivery at the door
+    text: string;
+    // a list of people like 'team'
+    contacts: string[];
+    // 根据时间生成的cron表达式，例如：每天上午8点执行任务则 cron表示为 0 0 8 * * *
+    cron:string;
+    // 时间周期，例如每天上午8点
+    time:string;
+};
+
+// 【2023年8月8日晚上10点】提醒群【全体员工】：明天上午8点去接王总
+export type ScheduledTasksRooMMessage = {
+    // Some message content, such as coming to the meeting immediately and picking up the delivery at the door
+    text: string;
+    // a list of people like 'team'
+    rooms: string[];
+    // 根据时间生成的cron表达式，例如：每天上午8点执行任务则 cron表示为 0 0 8 * * *
+    cron:string;
+    // 时间周期，例如每天上午8点
+    time:string;
+};
 
 // 通知【xxx】：会议取消了、告诉【xxx】：会议取消了
 export type Message = {
@@ -24,8 +48,14 @@ export type SendMessageAction = {
 
 export type SendRoomMessageAction = {
     // 向某个群发送消息或向某个群发通知
-    actionType: 'sendRoomMessage';
-    event: RoomMessage;
+    actionType: 'scheduledTasksRoomMessage';
+    event: ScheduledTasksRooMMessage;
+};
+
+export type ScheduledTasksMessageAction = {
+    // 定时向某个好友发送消息或向某个群发通知
+    actionType: 'scheduledTasksMessage';
+    event: ScheduledTasksMessage;
 };
 
 // if the user types text that can not easily be understood as a calendar action, this action is used
@@ -38,7 +68,9 @@ export interface UnknownAction {
 export type Action =
     | UnknownAction
     | SendRoomMessageAction
-    | SendMessageAction;
+    | SendMessageAction
+    | ScheduledTasksRooMMessage
+    | ScheduledTasksMessageAction;
 
 export type MessageActions = {
     actions: Action[];
